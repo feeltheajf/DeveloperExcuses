008a3779dc26d116b5651152ed4a17901bf82211
Ilya Radostev <radostev.ilya4955148@gmail.com>
2018-09-13 19:03:09 +0300

    Initial Commit

---
+++ DeveloperExcuses/DeveloperExcusesView.h
@@ -0,0 +1,13 @@
+//
+//  DeveloperExcusesView.h
+//  DeveloperExcuses
+//
+//  Created by ekzo! on 13/09/2018.
+//  Copyright © 2018 ekzo!. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DeveloperExcusesView : ScreenSaverView
+
+@end
